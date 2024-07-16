38715c532e4429ae80346618feb52f6174032afc
white-gardenia613 <2571358956@qq.com>
2024-07-16 17:54:54 +0800

    vue学习

---
+++ Vue/vue-project1/src/App.vue
@@ -0,0 +1,32 @@
+<!-- <script>
+export default{
+  setup() {
+    console.log('setup')
+    const message='this is message'
+    const logMessage=()=>{
+      console.log(message)
+    }
+    return{
+      message,
+      logMessage
+    }
+  },
+  beforeCreate() {
+    console.log('beforeCreate')
+  },
+}
+</script> -->
+
+<script setup>
+const message='this is message'
+const logMessage=()=>{
+  console.log(message)
+}
+</script>
+
+<template>
+  <div>
+    {{ message }}
+    <button @click="logMessage">log</button>
+  </div>
+</template>
