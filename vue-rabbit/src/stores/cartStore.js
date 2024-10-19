3cfe9b0313c46de3d394dd64982ee15572951436
baizhi613 <116872983+baizhi613@users.noreply.github.com>
2024-10-19 18:29:48 +0800

    小兔鲜商城项目

--- vue-rabbit/src/stores/cartStore.js
+++ vue-rabbit/src/stores/cartStore.js
@@ -1,32 +1,78 @@
 import { defineStore } from 'pinia'
 import { ref,computed } from 'vue'
-
+import {useUserStore} from './userStore'
+import {insertCartAPI,findNewCartListAPI,delCartAPI} from '@/apis/cart'
 
 export const useCartStore = defineStore('cart', () => {
+  const userStore=useUserStore()
+  const isLogin=computed(()=>userStore.userInfo.token)
   const cartList = ref([])
-  const addCart = (goods) => {
-    console.log('添加', goods)
-    const item = cartList.value.find((item) => goods.skuId === item.skuId)
-    if (item) {
-      item.count++
-    } else {
-      cartList.value.push(goods)
+
+  const updateNewList=async()=>{
+    const res=await findNewCartListAPI()
+    cartList.value=res.result
+  }
+
+  const addCart = async(goods) => {
+    const {skuId,count}=goods
+    if(isLogin.value){
+      await insertCartAPI({skuId,count})
+      updateNewList()
+    }else{
+      const item = cartList.value.find((item) => goods.skuId === item.skuId)
+      if (item) {
+        item.count++
+      } else {
+        cartList.value.push(goods)
+      }
     }
   }
 
-  const delCart=(skuId)=>{
-    const idx = cartList.value.findIndex((item) => skuId === item.skuId)
-    cartList.value.splice(idx, 1)
+  const delCart=async(skuId)=>{
+    if(isLogin.value){
+      await delCartAPI([skuId])
+      updateNewList()
+    }else{
+      const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+      cartList.value.splice(idx, 1)
+    }
+  }
+
+  const clearCart=()=>{
+    cartList.value=[]
+  }
+
+  const singleCheck=(skuId,selected)=>{
+    const item=cartList.value.find((item)=>item.skuId===skuId)
+    item.selected=selected
+  }
+
+  const allCheck=(selected)=>{
+    cartList.value.forEach(item=>item.selected=selected)
   }
 
   const allCount=computed(()=>cartList.value.reduce((a,c)=>a+c.count,0))
+
   const allPrice=computed(()=>cartList.value.reduce((a,c)=>a+c.count*c.price,0))
+
+  const selectedCount=computed(()=>cartList.value.filter(item=>item.selected).reduce((a,c)=>a+c.count,0))
+
+  const selectedPrice=computed(()=>cartList.value.filter(item=>item.selected).reduce((a,c)=>a+c.count*c.price,0))
+
+  const isAll=computed(()=>cartList.value.every((item)=>item.selected))
   return {
     cartList,
     addCart,
     delCart,
+    isAll,
     allCount,
-    allPrice
+    allPrice,
+    singleCheck,
+    allCheck,
+    selectedCount,
+    selectedPrice,
+    clearCart,
+    updateNewList
   }
 }, {
   persist: true,
